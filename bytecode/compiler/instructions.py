0c99877456124bf2ae384ec2b0c0a3db302b457f
loovjo <jonathan.loov@gmail.com>
2020-07-12 01:06:11 +0200

    Add all instructions

--- bytecode/compiler/instructions.py
+++ bytecode/compiler/instructions.py
@@ -25,7 +25,10 @@ class Instruction:
     __repr__ = __str__
 
 INSTRUCTION_PATTERNS = \
-    [ InstructionPattern("set_self_addr", bytes([0x00]))
+    [ InstructionPattern("dw", bytes([]))
+        .verify("needs one argument", DISPLAY_ALL, lambda args: len(args) == 1)
+        .verify("argument one has to be int", 0, lambda args: isinstance(args[0], Integer))
+    , InstructionPattern("set_self_addr", bytes([0x00]))
         .verify("needs one argument", DISPLAY_ALL, lambda args: len(args) == 1)
         .verify("argument one has to be register", 0, lambda args: isinstance(args[0], Register))
     , InstructionPattern("set_float", bytes([0x01]))
@@ -50,9 +53,35 @@ INSTRUCTION_PATTERNS = \
     , InstructionPattern("integer_to_atom", bytes([0x05]))
         .verify("needs one argument", DISPLAY_ALL, lambda args: len(args) == 1)
         .verify("argument one has to be register", 0, lambda args: isinstance(args[0], Register))
-    , InstructionPattern("dw", bytes([]))
-        .verify("needs one argument", DISPLAY_ALL, lambda args: len(args) == 1)
-        .verify("argument one has to be int", 0, lambda args: isinstance(args[0], Integer))
+
+    , InstructionPattern("add_int", bytes([0x10]))
+        .verify("needs two arguments", DISPLAY_ALL, lambda args: len(args) == 2)
+        .verify("argument one has to be register", 0, lambda args: isinstance(args[0], Register))
+        .verify("argument two has to be register", 1, lambda args: isinstance(args[1], Register))
+    , InstructionPattern("sub_int", bytes([0x11]))
+        .verify("needs two arguments", DISPLAY_ALL, lambda args: len(args) == 2)
+        .verify("argument one has to be register", 0, lambda args: isinstance(args[0], Register))
+        .verify("argument two has to be register", 1, lambda args: isinstance(args[1], Register))
+    , InstructionPattern("mul_int", bytes([0x12]))
+        .verify("needs two arguments", DISPLAY_ALL, lambda args: len(args) == 2)
+        .verify("argument one has to be register", 0, lambda args: isinstance(args[0], Register))
+        .verify("argument two has to be register", 1, lambda args: isinstance(args[1], Register))
+
+    , InstructionPattern("send_message", bytes([0x80]))
+        .verify("needs at least 4 arguments", DISPLAY_ALL, lambda args: len(args) >= 4)
+        .verify("argument one has to be register", 0, lambda args: isinstance(args[0], Register))
+        .verify("argument two has to be register", 1, lambda args: isinstance(args[1], Register))
+        .verify("argument three has to be register", 2, lambda args: isinstance(args[2], Register))
+        .verify("argument four has to be int", 3, lambda args: isinstance(args[3], Integer))
+        .verify("needs this many register-arguments", 3, lambda args: len(args) == args[3].inner.value + 4)
+        # TODO: Check that all register-arguments are registers, with good errors
+    , InstructionPattern("add_handler", bytes([0x81]))
+        .verify("needs at least 3 arguments", DISPLAY_ALL, lambda args: len(args) >= 3)
+        .verify("argument one has to be register", 0, lambda args: isinstance(args[0], Register))
+        .verify("argument two has to be integer", 1, lambda args: isinstance(args[1], Integer))
+        .verify("argument three has to be integer", 2, lambda args: isinstance(args[2], Integer))
+        .verify("needs this many register-arguments", 3, lambda args: len(args) == args[2].inner.value + 3)
+        # TODO: Check that all register-arguments are registers, with good errors
     # TODO: More instructions
     ]
 
