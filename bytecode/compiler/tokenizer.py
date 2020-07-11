ba17fbae09cc41727c65c93a0cc77bace96f1c72
loovjo <jonathan.loov@gmail.com>
2020-07-11 21:36:10 +0200

    Better parsing using a more combinator-based approach

--- bytecode/compiler/tokenizer.py
+++ bytecode/compiler/tokenizer.py
@@ -22,6 +22,11 @@ class ParseException(Exception):
         self.reason = reason
         self.span = span
 
+    def __str__(self):
+        return f"ParseException(reason={repr(self.reason)}, span={self.span})"
+
+    __repr__ = __str__
+
 class Token(ABC):
     def __init__(self, span):
         self.span = span
@@ -32,72 +37,85 @@ class Token(ABC):
 
 def take_while(fn):
     fn_ = lambda x: x != "" and fn(x)
-    class TakeWhile(Token):
-        def parse_one(inp):
-            if fn_(inp.peek().get()):
-                span, inp = inp.pop()
-                while fn_(inp.peek().get()):
-                    next, inp = inp.pop()
-                    span = span.combine(next)
+    def parse_one(inp):
+        if fn_(inp.peek().get()):
+            span, inp = inp.pop()
+            while fn_(inp.peek().get()):
+                next, inp = inp.pop()
+                span = span.combine(next)
 
-                return TakeWhile(span), inp
-            else:
-                raise ParseException("Not found", inp.peek())
+            return span, inp
+        else:
+            raise ParseException("Not found", inp.peek())
 
-    return TakeWhile
+    return parse_one
 
 def take_while0(fn):
     fn_ = lambda x: x != "" and fn(x)
-    class TakeWhile0(Token):
-        def parse_one(inp):
-            if fn_(inp.peek().get()):
-                span, inp = inp.pop()
-                while fn_(inp.peek().get()):
-                    next, inp = inp.pop()
-                    span = span.combine(next)
+    def parse_one(inp):
+        if fn_(inp.peek().get()):
+            span, inp = inp.pop()
+            while fn_(inp.peek().get()):
+                next, inp = inp.pop()
+                span = span.combine(next)
 
-                return TakeWhile0(span), inp
-            else:
-                return TakeWhile0(Span(inp.cursor, inp.cursor, inp.file_cont)), inp
+            return span, inp
+        else:
+            return Span(inp.cursor, inp.cursor, inp.file_cont), inp
 
-    return TakeWhile0
+    return parse_one
 
-Spaces = take_while0(lambda x: x in " \n")
+parse_spaces = take_while0(lambda x: x in " \n")
 
-def spaced(Token):
-    class Spaced(Token.__class__):
-        def parse_one(inp):
-            _, inp = Spaces.parse_one(inp)
-            token, inp = Token.parse_one(inp)
-            _, inp = Spaces.parse_one(inp)
+def spaced(parse_fn):
+    def parse_one(inp):
+        _, inp = parse_spaces(inp)
+        data, inp = parse_fn(inp)
+        _, inp = parse_spaces(inp)
 
-            return token, inp
+        return data, inp
 
-    return Spaced
+    return parse_one
 
 def find_exact(st):
-    class FindExact(Token):
-        def parse_one(inp):
-            if inp.file_cont[inp.cursor:inp.cursor + len(st)] == st:
-                span, inp = inp.pop()
-                for _ in range(len(st) - 1):
-                    span, inp = span.combine(inp.pop())
-                return FindExact(span), inp
-            raise ParseException(f"Expected {st}", inp.peek())
+    def parse_one(inp):
+        if inp.file_cont[inp.cursor:inp.cursor + len(st)] == st:
+            span, inp = inp.pop()
+            for _ in range(len(st) - 1):
+                span, inp = span.combine(inp.pop())
+            return span, inp
+        raise ParseException(f"Expected {st}", inp.peek())
 
-    return FindExact
+    return parse_one
 
-AssemblyInstructionToken = spaced(take_while(lambda ch: ch in "abcdefghijklmnopqrstuvwxyz_"))
+def map_res(res_wrapper, parse_fn):
+    def parse_one(inp):
+        data, inp = parse_fn(inp)
+
+        return res_wrapper(data), inp
+
+    return parse_one
+
+class AssemblyInstructionToken(Token):
+    def __init__(self, span, inst):
+        super(AssemblyInstructionToken, self).__init__(span)
+        self.inst = inst
+
+    @spaced
+    def parse_one(inp):
+        span, inp = spaced(take_while(lambda ch: ch in "abcdefghijklmnopqrstuvwxyz_"))(inp)
+
+        return AssemblyInstructionToken(span, span.get()), inp
 
 class Comment(Token):
     CommentStart = find_exact(";")
     UntilEOL = take_while(lambda x: x != "\n")
 
     def parse_one(inp):
-        comment_syntax, inp = Comment.CommentStart.parse_one(inp)
-        comment, inp = Comment.UntilEOL.parse_one(inp)
+        comment_syntax, inp = Comment.CommentStart(inp)
+        comment, inp = Comment.UntilEOL(inp)
 
-        return Comment(comment_syntax.span.combine(comment.span)), inp
+        return Comment(comment_syntax.combine(comment)), inp
 
 class Char(Token):
     def __init__(self, span, value):
@@ -129,15 +147,15 @@ class String(Token):
         self.value = value
 
     START_END = find_exact("'")
+    @spaced
     def parse_one(inp):
-        start, inp = String.START_END.parse_one(inp)
-        span = start.span
+        span, inp = String.START_END(inp)
 
         value = ''
         while True:
             try:
-                end, inp = String.START_END.parse_one(inp)
-                span = span.combine(end.span)
+                end, inp = String.START_END(inp)
+                span = span.combine(end)
                 break
             except ParseException as _:
                 ch, inp = Char.parse_one(inp)
@@ -153,42 +171,62 @@ class Integer(Token):
 
     NUMBER = take_while(lambda x: x in "0123456789")
     def parse_one(inp):
-        number, inp = Integer.NUMBER.parse_one(inp)
+        number, inp = Integer.NUMBER(inp)
 
-        return Integer(number.span, int(number.span.get())), inp
+        return Integer(number, int(number.get())), inp
 
 class Float(Token):
     def __init__(self, span, value):
         super(Float, self).__init__(span)
         self.value = value
 
+    @spaced
     def parse_one(inp):
         # TODO: This currently breaks on things like 1e5, because it assumes a prefix of a valid
         # float is a valid float. '1e' is not a valid float
         longest_flength = 0
+        value = None
         for flength in range(1, len(inp.file_cont) - inp.cursor):
             try:
                 st = inp.file_cont[inp.cursor:inp.cursor+flength]
-                if " " in st:
+                if " " in st or "\n" in st:
                     raise ValueError()
                 value = float(st)
                 longest_flength = flength
             except ValueError as _:
-                if inp.file_cont[inp.cursor+flength-1] == "e":
-                    continue
                 break
+
+        if value is None:
+            raise ParseError("Expected float", Span(inp.cursor, inp.cursor + 1, inp.file_cont))
 
         span = Span(inp.cursor, inp.cursor + longest_flength, inp.file_cont)
         inp.cursor += longest_flength
 
         return Float(span, value), inp
 
-inp_text = r"123 yo"
+inp_text = r"""
+7.5
+123
+'hello\'123\n'
+; hello
+yo_yo
+"""
 inp_pi = ParseInput(inp_text)
 try:
     inst, inp_pi = Float.parse_one(inp_pi)
     inst.span.print_aa()
     print(inst.value)
+
+    inst, inp_pi = Integer.parse_one(inp_pi)
+    inst.span.print_aa()
+    print(inst.value)
+
+    inst, inp_pi = String.parse_one(inp_pi)
+    inst.span.print_aa()
+    print(inst.value)
+
+    inst, inp_pi = Comment.parse_one(inp_pi)
+    inst.span.print_aa()
 
     inst, inp_pi = AssemblyInstructionToken.parse_one(inp_pi)
     inst.span.print_aa()
