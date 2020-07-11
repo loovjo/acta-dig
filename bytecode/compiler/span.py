4faf4f7219804ca95a542821eba07b639d799b93
loovjo <jonathan.loov@gmail.com>
2020-07-11 17:48:49 +0200

    Change structure a bit, fix off by one error

--- bytecode/compiler/span.py
+++ bytecode/compiler/span.py
@@ -1,5 +1,3 @@
-from abc import ABC, abstractmethod
-
 SPAN_COL = "\033[38;5;16m"
 RESET_COL = "\033[0m"
 
@@ -7,16 +5,15 @@ def lpad(st, wanted_len, pad_ch=" "):
     return pad_ch * (wanted_len - len(st)) + st
 
 class Span:
-    def __init__(self, start, end):
+    def __init__(self, start, end, source):
         self.start = start
         self.end = end
+        self.source_lines = source.split("\n")
 
-    def print_aa(self, file_cont):
-        lines = file_cont.split("\n")
-
+    def print_aa(self):
         # Find start line
         line_start = 0
-        for i, line in enumerate(lines):
+        for i, line in enumerate(self.source_lines):
             line += "\n"
             if line_start + len(line) > self.start:
                 start_line = i
@@ -26,7 +23,7 @@ class Span:
 
         # Find start line
         line_end = 0
-        for i, line in enumerate(lines):
+        for i, line in enumerate(self.source_lines):
             line += "\n"
 
             line_end += len(line)
@@ -36,18 +33,18 @@ class Span:
 
 
         offset_into_first_line = self.start - line_start
-        offset_into_last_line = self.end - line_end + len(lines[end_line])
+        offset_into_last_line = self.end - line_end + len(self.source_lines[end_line])
 
         linenr_len = len(str(end_line))
 
-        print(SPAN_COL + f"  At lines {start_line+1}-{end_line+1}")
+        print(SPAN_COL + f"  At self.source_lines {start_line+1}-{end_line+1}")
 
         if start_line == end_line:
             print(
                 SPAN_COL +
-                lpad(str(start_line), linenr_len) +
+                lpad(str(start_line+1), linenr_len) +
                 " │",
-                RESET_COL + lines[start_line]
+                RESET_COL + self.source_lines[start_line]
             )
             print(
                 SPAN_COL +
@@ -66,7 +63,13 @@ class Span:
                 "╮"
             )
             for line_idx in range(start_line, end_line + 1):
-                print(SPAN_COL + lpad(str(line_idx), linenr_len) + " │", RESET_COL + lines[line_idx])
+                print(
+                    SPAN_COL +
+                    lpad(str(line_idx+1), linenr_len) +
+                    " │",
+                    RESET_COL +
+                    self.source_lines[line_idx]
+                )
             print(
                 SPAN_COL +
                 " " * linenr_len +
@@ -79,6 +82,6 @@ class Span:
 if __name__ == "__main__":
     file_cont = open(__file__).read()
 
-    span = Span(100, 105)
-    span.print_aa(file_cont)
+    span = Span(100, 105, file_cont)
+    span.print_aa()
     print(file_cont[span.start], file_cont[span.end])
