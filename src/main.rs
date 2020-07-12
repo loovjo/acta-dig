6c07e301dc2db8981e18a82b8185c7dcffe126bb
loovjo <jonathan.loov@gmail.com>
2020-07-12 23:48:14 +0200

    Sample actor

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,43 @@
-mod messages;
 mod acting;
+mod messages;
 
 mod actors;
-use actors::{dyn_actor, io_actor};
+use actors::{dactor, io_actor};
 
 mod vm;
-
-const ATOM_START: messages::Atom = messages::Atom(0);
 
 fn main() {
     let mut worker = acting::Worker::new();
 
     let io_addr = worker.add_actor(Box::new(io_actor::IOActor));
 
-    let dyn_actor = construct_dyn();
+    let dactor = dactor::Dactor::new(make_base());
 
-    let dyn_addr = worker.add_actor(Box::new(dyn_actor));
+    let dactor_addr = worker.add_actor(Box::new(dactor));
 
     eprintln!("Registered io actor at {:?}", io_addr);
-    eprintln!("Registered dyn actor at {:?}", dyn_addr);
+    eprintln!("Registered dyn actor at {:?}", dactor_addr);
 
-    worker.msg_queue.make_ctx(messages::ActorAddr::random()).push_msg(messages::Message {
-        to: dyn_addr,
-        cont: messages::MessageContent {
-            atom: ATOM_START,
-            data: vec![
-                messages::Value::ActorAddr(io_addr),
-                messages::Value::Atom(io_actor::IOActor::PRINT_MSG),
-            ],
-        },
-        arrive_after: None,
-    });
+    worker
+        .msg_queue
+        .make_ctx(messages::ActorAddr::random())
+        .push_msg(messages::Message {
+            to: dactor_addr,
+            cont: messages::MessageContent {
+                atom: messages::Atom(0x333301), // get-counter
+                data: vec![
+                    messages::Value::ActorAddr(io_addr),
+                    messages::Value::Atom(io_actor::IOActor::PRINT_MSG),
+                ],
+            },
+            arrive_after: None,
+        });
 
     while worker.step_once() {}
 }
 
-fn construct_dyn() -> dyn_actor::DynActor {
-    let mut actor_fns = std::collections::HashMap::new();
-
-    let fn_1 = move |msg_content: messages::MessageContent, mut context: acting::Context| {
-        match &*msg_content.data {
-            &[messages::Value::ActorAddr(io_addr), messages::Value::Atom(ref atom)] => {
-                context.push_msg(messages::Message {
-                    to: io_addr,
-                    cont: messages::MessageContent {
-                        atom: *atom,
-                        data: vec![messages::Value::String("Hello, world".to_string())],
-                    },
-                    arrive_after: Some(
-                        std::time::Instant::now() + std::time::Duration::from_secs(1),
-                    ),
-                });
-            }
-            _ => {}
-        }
-    };
-
-    let fn_1_boxed: Box<dyn Fn(messages::MessageContent, acting::Context)> = Box::new(fn_1);
-
-    actor_fns.insert(ATOM_START, ("START".to_string(), fn_1_boxed));
-
-    dyn_actor::DynActor {
-        actor_name: "Starter Boi".to_string(),
-        actor_fns,
-    }
+fn make_base() -> &'static vm::dactor_base::DactorBase {
+    let base = vm::dactor_base::DactorBase::parse(include_bytes!("simple_actor.act"))
+        .expect("no parse :(");
+    Box::leak(Box::new(base)) // not pog
 }
